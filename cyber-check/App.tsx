e0fa7266124681b8fdcd2799e2a18b3e36d86115
Bryan Barrera <82698237+bryanbarreracortes@users.noreply.github.com>
2022-11-06 23:04:58 -0800

    [OD-80] Added Recent Reports Screen (#7)
    * Added Recent Reports Screen
    
    * Yarn.lock removed

--- cyber-check/App.tsx
+++ cyber-check/App.tsx
@@ -1,6 +1,8 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet,View,Text } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { StatusBar } from "expo-status-bar";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 import Signin from "./src/screens/SignIn";
 import Backend from "./src/screens/Backend";
@@ -28,8 +30,44 @@ const App = () => {
           />
         </Stack.Navigator>
       </NavigationContainer>
+    <NavigationContainer>
+      <View style={styles.container}>
+        <Text>Welcome to Cybercheck!</Text>
+        <StatusBar style="auto" />
+      </View>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Recent Reports" component={RecentReportsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
     </>
+
+  );
+}
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Home!</Text>
+    </View>
   );
 };
 
 export default App;
+function RecentReportsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Recent Reports!</Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
