98ba32df778ffe34585e15e94388aee125d207fd
pokemongt <whisperingguys@gmail.com>
2023-02-11 14:31:55 -0800

    updated Feb 11

--- cyber-check/src/screens/ReportDescription.tsx
+++ cyber-check/src/screens/ReportDescription.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, View, Text, Pressable, Dimensions,TextInput , ImageBackground, Platform, StatusBar, TouchableOpacity, Falsy, RecursiveArray, RegisteredStyle, ViewStyle } from "react-native";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Icon } from "@rneui/base";
+import { Input } from "@rneui/themed";
 
 type RootStackParamList = {};
 
@@ -13,6 +14,8 @@ const windowHeight = Dimensions.get("window").height;
 
 const ReportDescription = ({ navigation, route }: Props) => {
   let {reportName} = route.params 
+  const [InputText, setInputText] = useState("");
+  const [CommentText, setCommentText] = useState("");
 
   return ( 
 
@@ -26,30 +29,52 @@ const ReportDescription = ({ navigation, route }: Props) => {
             </TouchableOpacity>
           </View>
           
-         <View style={{flex: 1, justifyContent: "right", alignItems: "rtl"}}> 
+          <View style={{flex: 20, alignItems: "center", justifyContent: "center"}}> 
+            <Text style={{fontSize: 30,fontWeight: "bold"}}> {reportName}</Text>
+          </View>
+          <View style={{flex: 1, flexDirection: 'row', justifyContent: 'flex-end', alignItems: 'center'}}> 
             <TouchableOpacity onPress={() => navigation.navigate("", {reportName})}>
               <Icon name="share" type="material"></Icon>
             </TouchableOpacity>
           </View>
-          <View style={{flex: 20, alignItems: "center", justifyContent: "center"}}> 
-            <Text style={{fontSize: 30,fontWeight: "bold"}}> {reportName}</Text>
-          </View>
         </View> 
         
         <View style={{flex: 5}}>
-        <View style={{flex: 1, alignItems: "center", justifyContent: "center", paddingLeft: 10, paddingRight: 10}}>
-          <Text style={{fontSize: 20, fontWeight: "bold"}} > Task Name: </Text>
+        <View style={{ alignItems: "center", justifyContent: "center"}}>
+          <Text style={{fontSize: 20, fontWeight: "bold"}} > Task Name: testing </Text>
+        </View > 
+        <View >
+          <TextInput
+            multiline={true}
+    
+            style={styles.description}
+            onChangeText={(description) => setInputText(description)}
+            placeholder="Description..."
+            value={InputText}>
+          </TextInput>       
+        </View>
+      <View style ={{}}>
+          <Text style={{fontSize: 20, fontWeight:"bold"}} > Completed by: Billy</Text>
+
           </View> 
-      
-       
-        <View style={{flex: 10, paddingLeft: 10, paddingRight: 10}}>
-        
-        <Text style={{fontSize: 20, fontWeight: "bold"}} > Description: </Text>
-          <Border style={{borderWidth:  1}}>
-            <Text style={{fontSize: 25, borderColor: "black", fontWeight: "300"}} > This is a test and a Dummy Task Description </Text>
-          </Border>         
-        </View>
-        </View>
+      <View>
+          <Text style={{fontSize: 20,fontWeight:"bold"}} > On (insert date and time)</Text>
+        </View> 
+      <View>
+      <TextInput
+          multiline={true}
+          numberOfLines={4}
+          style={styles.Comment}
+          onChangeText={(Comment) => setCommentText(Comment)}
+          placeholder="comments..."
+          value={CommentText}>
+        </TextInput>  
+      </View>
+
+
+
+
+      </View>
         
         <View style={styles.container}>
           <Pressable
@@ -99,13 +124,35 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
   },
   button: {
+    marginTop: 30,
+    padding: 10,
     backgroundColor: "#007AFF",
-    alignItems: "center",
-    justifyContent: "center",
-    borderRadius: 10,
-    width: windowWidth * 1,
-    height: windowHeight * .05
+    width: 150,
+    height: 50,
+    borderRadius: 25,
+    alignItems: 'center',
+    justifyContent: 'center'
   },
+  description: {
+    marginTop: 20,
+    padding: 10,
+    borderWidth: 1,
+    borderRadius: 10,
+    minWidth: windowWidth * 0.8,
+    height: 300,
+    fontSize: 20,
+    backgroundColor: "rgba(217, 217, 217, 0.25)",
+  },
+  Comment: {
+    marginTop: 10,
+    padding: 10,
+    borderWidth: 1,
+    borderRadius: 10,
+    minWidth: windowWidth * 0.8,
+    height: 150,
+    fontSize: 20,
+    backgroundColor: "rgba(217, 217, 217, 0.25)",
+  }
 });
 
 export default ReportDescription;
