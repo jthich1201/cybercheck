7c1ab0f8e6941c59a0e1bb6fe2137a09630216d8
rmao-csus <90656642+rmao-csus@users.noreply.github.com>
2023-02-28 23:16:53 -0800

    merege Od 44 create model for reports (#27)
    * added google sso
    
    * moved user info to home tab
    
    * started adding azure auth
    
    * microsoft sso working on ios, isses w/ android
    
    * sso working(android not)
    
    * added files
    
    * android build fixed
    
    * added user table or somethig
    
    * Created Reports table
    
    * Save User DAta
    
    * added admin role to user when creating admin account
    
    * Fixed misspelling on UserController
    
    * added user table or somethig
    
    * dependency fix
    
    * added json file with prompts
    
    * sso working(android not)
    
    * dependency fix
    
    * sso working(android not)
    
    * dependency fix
    
    * dependency fix
    
    * dependency fix
    
    * sso working(android not)
    
    * dependency fix
    
    * dependency fix
    
    * dependency fix
    
    * sso working(android not)
    
    * dependency fix
    
    * pls fix merge plsplsplppllp ladfslpdfsf
    
    ---------
    
    Co-authored-by: mudk01 <khalid.mueed01@gmail.com>
    Co-authored-by: AndyNeedsHelp <andrewzavala30@gmail.com>
    Co-authored-by: Rachel Mao <rachelrmao99@hotmail.com>
    Co-authored-by: pokemongt <whisperingguys@gmail.com>
    Co-authored-by: Bryan Barrera <bryanbarreracortes@csus.edu>
    Co-authored-by: Rafaela Lomboy <rafaela.lomboy@gmail.com>

--- server/src/db/Create-Tables.ts
+++ server/src/db/Create-Tables.ts
@@ -2,7 +2,7 @@ import incidentResponseDbPool from "./incidentResponseDb";
 
 
 
-export const userTable =async () => {
+export const userTable = async () => {
     const result = await incidentResponseDbPool.query(`CREATE TABLE IF NOT EXISTS users (
 id uuid,
 name varchar(65535),
@@ -12,5 +12,19 @@ platform_id uuid,
 role varchar(100),
 created_at timestamp,
 updated_at timestamp)`)
-return result
+    return result
+}
+
+export const reportsTable = async () => {
+    const result = await incidentResponseDbPool.query(`CREATE TABLE IF NOT EXISTS reports (
+id uuid,
+title varchar,
+creator uuid,
+created_at timestamp,
+updated_at timestamp,
+type varchar,
+status varchar,
+org_id uuid,
+group_id`)
+    return result
 }
