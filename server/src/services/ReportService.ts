54eaa2210603d3b9425f518d0cd2a9b6871abd93
mudk01 <khalid.mueed01@gmail.com>
2023-04-08 23:40:33 -0700

    updated report creation, task filtering on severity/incident_type, task creation

--- server/src/services/ReportService.ts
+++ server/src/services/ReportService.ts
@@ -1,22 +1,21 @@
 import incidentResponseDbPool from "../db/incidentResponseDb";
 import { v4 as uuidv4 } from 'uuid';
 
+export const createReport = async (title: string, creator: string, type: string, status: string, orgId: string, groupId: string) => {
+  const id = uuidv4();
+  const created_at = new Date();
+  const result = await incidentResponseDbPool.query("INSERT INTO reports (report_id, title, creator, created_at, type, status, org_id, group_id, updated_at) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *",
+    [id, title, creator, created_at, type, status, orgId, groupId, created_at]);
+  return result.rows;
+};
+
+export const getReports = async (userId: any) => {
+  const result = await (incidentResponseDbPool.query("Select report_id, title from reports where report_id in (Select report_id from report_members where user_id = $1)",
+    [userId]));
+  return result.rows;
+};
 
 
-  export const createReport = (name: string, incidentType: string ) => {
-    const id = uuidv4();
-    const result = incidentResponseDbPool.query("INSERT INTO reports (name, incidentType, id) VALUES ($1, $2, $3) RETURNING *",
-     [name, incidentType, id]);
-    return result;
-  };
-
-  export const getReports = async(userId: any ) => {
-    const result = await(incidentResponseDbPool.query("Select report_id, title from reports where report_id in (Select report_id from report_members where user_id = $1)",
-      [userId]));
-    return result.rows;
-  };
-
-  
     //  export const saveChanges = (name: string, incidentType: string, comment: string, description: string ) => {
     //   const id = uuidv4();
     //   const result = incidentResponseDbPool.query("INSERT INTO reports (name, incidentType, id, comment, description) VALUES ($1, $2, $3) RETURNING *",
